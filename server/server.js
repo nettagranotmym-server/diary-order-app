da2bcb27dae39c7efbf0c2085bbc83b1db862cce
Netta Granot <netta.granot.mym@gmail.com>
2025-07-19 11:30:36 +0300

    fix: matching filename case for order model

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Order = require('./models/Order');
+const Order = require('./models/order');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
